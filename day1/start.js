2e26125f83e38407c5af52db2be08fcd82ab1790
Apirat Chiwakanit <apirat.c@kbtg.tech>
2020-03-18 11:16:26 +0700

    get coordinates array

--- day1/start.js
+++ day1/start.js
@@ -1,3 +1,39 @@
 (() => {
     // day 1
+    function setup() {
+        const canvas = document.getElementById('falling-snow');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        return {
+            canvas,
+            canvasContext: canvas.getContext('2d'),
+            numberOfSnowBalls: 250
+        }
+    }
+
+    function random(min, max) {
+        // สุ่มเลขระหว่าง min - max
+        // (1) (max - min) +1
+        // (2) (1) * random 0 - 0.99
+        // (3) (2) ปัดเศษลง
+        // (4) (3) + min
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function createSnowBalls(canvas, numberOfSnowBalls) {
+        const coordinates = [...Array(numberOfSnowBalls)].map(() => {
+            return {
+                x: random(0, canvas.width),
+                y: random(0, canvas.height)
+            }
+        });
+        return coordinates;
+    }
+
+    function run() {
+        const { canvas, canvasContext, numberOfSnowBalls } = setup();
+        createSnowBalls(canvas, numberOfSnowBalls);
+    }
+    run();
 })();
