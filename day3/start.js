5d5b941652200570877fb8601b1cfd946f6fa8c5
zobry <apirat.chiwa@gmail.com>
2020-03-28 13:17:09 +0700

    test Asynchronous

--- day3/start.js
+++ day3/start.js
@@ -1,6 +1,14 @@
 ;(() => {
 	// เริ่มเขียนโค้ด
 	// 1. How Asynchronous code works in JavaScript
+	function simulateAsyncAPI(text, timeout) {
+		setTimeout(() => {
+			console.log(text)
+		}, timeout)
+	}
+	simulateAsyncAPI('a', 1000)
+	simulateAsyncAPI('b', 500)
+	simulateAsyncAPI('c', 100)
 	// 2. Callback
 	// 3. Promise
 	// 4. Async/Await
