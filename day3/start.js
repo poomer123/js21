9fbfc0f83373ac9e788ed998a16cd2049184e40f
zobry <apirat.chiwa@gmail.com>
2020-03-28 13:31:23 +0700

    sample async await

--- day3/start.js
+++ day3/start.js
@@ -24,26 +24,43 @@
 	// })
 
 	// 3. Promise
-	function simulateAsyncAPI(text, timeout) {
-		return new Promise((resolve, reject) => {
-			setTimeout(() => {
-				if (text === 'b') {
-					return reject('b got rejected')
-				}
-				console.log(text)
-				resolve()
-			}, timeout)
-		})
-	}
-	simulateAsyncAPI('a', 1000)
-		.then(() => {
-			return simulateAsyncAPI('b', 500)
-		})
-		.then(() => {
-			return simulateAsyncAPI('c', 100)
-		})
-		.catch(error => {
-			console.log(error)
-		})
+	// function simulateAsyncAPI(text, timeout) {
+	// 	return new Promise((resolve, reject) => {
+	// 		setTimeout(() => {
+	// 			if (text === 'b') {
+	// 				return reject('b got rejected')
+	// 			}
+	// 			console.log(text)
+	// 			resolve()
+	// 		}, timeout)
+	// 	})
+	// }
+	// simulateAsyncAPI('a', 1000)
+	// 	.then(() => {
+	// 		return simulateAsyncAPI('b', 500)
+	// 	})
+	// 	.then(() => {
+	// 		return simulateAsyncAPI('c', 100)
+	// 	})
+	// 	.catch(error => {
+	// 		console.log(error)
+	//     })
+
 	// 4. Async/Await
+	function simulateAsyncAPI(text, timeout) {
+		setTimeout(() => {
+			console.log(text)
+		}, timeout)
+	}
+
+	async function run() {
+		try {
+			await simulateAsyncAPI('a', 1000)
+			await simulateAsyncAPI('b', 500)
+			await simulateAsyncAPI('c', 100)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	run()
 })()
