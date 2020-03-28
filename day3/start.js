c43b0e9721925f3bda76c58c45277f50b39b594e
zobry <apirat.chiwa@gmail.com>
2020-03-28 13:20:56 +0700

    sample callback

--- day3/start.js
+++ day3/start.js
@@ -1,15 +1,27 @@
 ;(() => {
 	// เริ่มเขียนโค้ด
 	// 1. How Asynchronous code works in JavaScript
-	function simulateAsyncAPI(text, timeout) {
+	// function simulateAsyncAPI(text, timeout) {
+	// 	setTimeout(() => {
+	// 		console.log(text)
+	// 	}, timeout)
+	// }
+	// simulateAsyncAPI('a', 1000)
+	// simulateAsyncAPI('b', 500)
+	// simulateAsyncAPI('c', 100)
+
+	// 2. Callback
+	function simulateAsyncAPI(text, timeout, callback) {
 		setTimeout(() => {
 			console.log(text)
+			callback()
 		}, timeout)
 	}
-	simulateAsyncAPI('a', 1000)
-	simulateAsyncAPI('b', 500)
-	simulateAsyncAPI('c', 100)
-	// 2. Callback
+	simulateAsyncAPI('a', 1000, () => {
+		simulateAsyncAPI('b', 500, () => {
+			simulateAsyncAPI('c', 100, () => {})
+		})
+	})
 	// 3. Promise
 	// 4. Async/Await
 })()
