287c9ad0b748b09edaa8eebd3d14515473de2f96
zobry <apirat.chiwa@gmail.com>
2020-03-30 11:34:54 +0700

    konami code

--- day7/start.js
+++ day7/start.js
@@ -1,3 +1,26 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const konamiCode = [
+		'ArrowUp',
+		'ArrowUp',
+		'ArrowDown',
+		'ArrowDown',
+		'ArrowLeft',
+		'ArrowRight',
+		'ArrowLeft',
+		'ArrowRight',
+		'b',
+		'a'
+	]
+	let index = 0
+	function onKeydown(event) {
+		event.key === konamiCode[index] ? index++ : (index = 0)
+		console.log(event.key)
+		if (konamiCode.length === index) {
+			startSnowing()
+		}
+	}
+	function run() {
+		document.addEventListener('keydown', onKeydown)
+	}
+	run()
 })()
