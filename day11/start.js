a5d66f1d9ce853f7cd458665f07bf6fc6883734c
zobry <apirat.chiwa@gmail.com>
2020-03-31 14:52:30 +0700

    check scroll and update class name

--- day11/start.js
+++ day11/start.js
@@ -1,6 +1,20 @@
 ;(() => {
+	function onScroll() {
+		const sectionElems = Array.from(document.querySelectorAll('section'))
+		sectionElems.forEach(section => {
+			const img = section.querySelector('img')
+			const text = section.querySelector('.text')
+
+			const scrollPosition = window.pageYOffset
+			const revealPosition = img.offsetTop + img.offsetHeight / 10
+			if (scrollPosition >= revealPosition) {
+				text.classList.add('reveal')
+			}
+		})
+	}
+
 	function run() {
-		console.log('run')
+		document.addEventListener('scroll', onScroll)
 	}
 	run()
 })()
