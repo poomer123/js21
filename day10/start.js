6794a55921cb615fadc47d6c43887a5fab5d3ede
zobry <apirat.chiwa@gmail.com>
2020-03-31 13:41:37 +0700

    implement drag drop elements

--- day10/start.js
+++ day10/start.js
@@ -1,3 +1,37 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	let draggingElem
+	function onDragStart() {
+		draggingElem = this
+	}
+
+	function onDrop(params) {
+		console.log(params)
+	}
+
+	function onDragOver(event) {
+		event.preventDefault()
+	}
+
+	function onDragEnter(event) {
+		event.preventDefault()
+	}
+
+	function run() {
+		const taskElems = Array.from(document.querySelectorAll('.task'))
+		const dropZoneElems = Array.from(
+			document.querySelectorAll('.drop-zone')
+		)
+
+		taskElems.forEach(task => {
+			task.addEventListener('dragstart', onDragStart)
+		})
+
+		dropZoneElems.forEach(dropZomeElem => {
+			dropZomeElem.addEventListener('drop', onDrop)
+			dropZomeElem.addEventListener('dragover', onDragOver)
+			dropZomeElem.addEventListener('dragenter', onDragEnter)
+		})
+	}
+
+	run()
 })()
