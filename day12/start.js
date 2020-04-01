3d5240d46a3aae5c30265db384024dbbf4450b7a
zobry <apirat.chiwa@gmail.com>
2020-04-01 17:17:25 +0700

    get data from api

--- day12/start.js
+++ day12/start.js
@@ -1,3 +1,20 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const key = 'iqair_key'
+
+	async function getAirQuality({ city, state, country }) {
+		const response = await fetch(
+			`https://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=${key}`
+		)
+		const result = await response.json()
+		console.log(result)
+	}
+
+	function run() {
+		const city = 'Sathon'
+		const state = 'Bangkok'
+		const country = 'Thailand'
+
+		getAirQuality({ city, state, country })
+	}
+	run()
 })()
