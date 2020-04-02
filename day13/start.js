7b9e4b1004a0a925c1fcc458eac83f01a93d18ae
zobry <apirat.chiwa@gmail.com>
2020-04-02 14:25:28 +0700

    not a number type

--- day13/start.js
+++ day13/start.js
@@ -1,6 +1,18 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
 	// 1. NaN
+	if (NaN === NaN || NaN * 1 === NaN || NaN / NaN === 1) {
+		console.log('Equal')
+	}
+
+	// make Nan
+	const result = 1 / 'hello'
+	if (result === NaN) {
+		console.log('Equal to Nan')
+	}
+	if (Number.isNaN(result)) {
+		console.log('Equal to Nan')
+	}
+
 	// 2. Type Coercion
 	// 3. Interpreter & Compiler
 	// 4. Checking Object Type
