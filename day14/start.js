d052a26134b016314e9e617f1930c0b2e850e572
zobry <apirat.chiwa@gmail.com>
2020-04-02 15:23:01 +0700

    scrollIntoView to make carousel

--- day14/start.js
+++ day14/start.js
@@ -1,3 +1,32 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	let currectIndex = 0
+
+	function displayImage(imageElems, newIndex) {
+		const lastIndex = imageElems.length - 1
+
+		if (newIndex < 0) {
+			newIndex = lastIndex
+		} else if (newIndex > lastIndex) {
+			newIndex = 0
+		}
+
+		const newImage = imageElems[newIndex]
+		newImage.scrollIntoView({ behavior: 'smooth' })
+		currectIndex = newIndex
+	}
+
+	function run() {
+		const imageElems = document.querySelectorAll('img')
+		const previousBtnElem = document.querySelector('.previous')
+		const nextBtnElem = document.querySelector('.next')
+
+		previousBtnElem.addEventListener('click', () =>
+			displayImage(imageElems, currectIndex - 1)
+		)
+
+		nextBtnElem.addEventListener('click', () => {
+			displayImage(imageElems, currectIndex + 1)
+		})
+	}
+	run()
 })()
