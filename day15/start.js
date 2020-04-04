b89e66deb5dcfc09a1b1ee262c16b47a79eb9ac1
zobry <apirat.chiwa@gmail.com>
2020-04-04 13:34:08 +0700

    play and pause song

--- day15/start.js
+++ day15/start.js
@@ -1,3 +1,23 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const audioElem = document.querySelector('audio')
+	const playBtnElem = document.querySelector('.play')
+	const progressBarElem = document.querySelector('.progress-bar')
+	const startTimeElem = document.querySelector('.start-time')
+	const endTimeElem = document.querySelector('.end-time')
+
+	function onClick() {
+		if (audioElem.paused) {
+			audioElem.play()
+			playBtnElem.className = 'pause'
+		} else {
+			audioElem.pause()
+			playBtnElem.className = 'play'
+		}
+	}
+
+	function run() {
+		playBtnElem.addEventListener('click', onClick)
+	}
+
+	run()
 })()
