ee08254ac4abba39ffa3f62b08e060180651bd78
zobry <apirat.chiwa@gmail.com>
2020-04-04 15:24:35 +0700

    update duration

--- day15/start.js
+++ day15/start.js
@@ -5,6 +5,21 @@
 	const startTimeElem = document.querySelector('.start-time')
 	const endTimeElem = document.querySelector('.end-time')
 
+	function getDuration(time = '00.000000') {
+		// คำนวณนาที และ วินาที
+		const minutes = Math.floor((time / 60) % 60).toString()
+		const seconds = Math.floor(time % 60)
+			.toString()
+			.padStart(2, '0')
+
+		return `${minutes}:${seconds}`
+	}
+
+	function onTimeUpdate() {
+		const { currentTime } = audioElem
+		startTimeElem.innerHTML = getDuration(currentTime)
+	}
+
 	function onClick() {
 		if (audioElem.paused) {
 			audioElem.play()
@@ -17,6 +32,7 @@
 
 	function run() {
 		playBtnElem.addEventListener('click', onClick)
+		audioElem.addEventListener('timeupdate', onTimeUpdate)
 	}
 
 	run()
