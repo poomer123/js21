c94ecc14bd4f0fdaefba8eabac22fc7c71cc3afc
zobry <apirat.chiwa@gmail.com>
2020-04-04 20:17:42 +0700

    text to speech ko-KR

--- day16/start.js
+++ day16/start.js
@@ -1,3 +1,25 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const message = new SpeechSynthesisUtterance()
+
+	function onVoicesChanged() {
+		const voices = speechSynthesis.getVoices()
+		const thVoice = voices.find(voice => {
+			return voice.lang === 'ko-KR'
+		})
+		message.voice = thVoice
+	}
+
+	function onClick(event) {
+		message.text = event.target.getAttribute('alt')
+		speechSynthesis.speak(message)
+	}
+
+	function run() {
+		speechSynthesis.addEventListener('voiceschanged', onVoicesChanged)
+
+		const imgElems = Array.from(document.querySelectorAll('img'))
+		imgElems.forEach(img => img.addEventListener('click', onClick))
+	}
+
+	run()
 })()
