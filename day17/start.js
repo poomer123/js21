b6c1fbce9b94dbe315bcc15f79989dff2d1c2c10
zobry <apirat.chiwa@gmail.com>
2020-04-06 16:45:21 +0700

    start - stop record

--- day17/start.js
+++ day17/start.js
@@ -1,3 +1,28 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	window.SpeechRecognition =
+		window.SpeechRecognition || window.webkitSpeechRecognition
+
+	const recognition = new window.SpeechRecognition()
+	const btnElem = document.querySelector('.control')
+
+	function onClick() {
+		const isPausing = btnElem.classList.contains('record')
+
+		if (isPausing) {
+			recognition.start()
+			btnElem.classList.remove('record')
+			btnElem.classList.add('pause')
+		} else {
+			recognition.stop()
+			btnElem.classList.remove('pause')
+			btnElem.classList.add('record')
+		}
+	}
+
+	function run() {
+		recognition.lang = 'th-TH'
+		btnElem.addEventListener('click', onClick)
+	}
+
+	run()
 })()
