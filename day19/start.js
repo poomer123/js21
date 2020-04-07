6e0460523b73cf48e698c55d7cea5e00b507065a
zobry <apirat.chiwa@gmail.com>
2020-04-07 15:48:00 +0700

    sample auto complete

--- day19/start.js
+++ day19/start.js
@@ -1,3 +1,50 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const carBrands = [
+		'BMW',
+		'Maserati',
+		'Mercedes Benz',
+		'Ferrari',
+		'Toyota',
+		'Honda',
+		'Hyundai',
+	]
+
+	const searchElem = document.querySelector('.search')
+
+	function clearResults() {
+		const ulElem = document.querySelector('.results')
+		if (ulElem) {
+			document.body.removeChild(ulElem)
+		}
+	}
+
+	function onSelectCarBrand(event) {
+		searchElem.value = event.target.innerText
+		clearResults()
+	}
+
+	function onInput(event) {
+		const inputText = event.target.value.toLowerCase()
+		const matchedCarBrand = carBrands.filter((carBrand) =>
+			carBrand.toLowerCase().startsWith(inputText)
+		)
+		if (matchedCarBrand.length > 0) {
+			const ulElem = document.createElement('ul')
+			ulElem.classList.add('results')
+			matchedCarBrand.forEach((carBrand) => {
+				const liElem = document.createElement('li')
+				liElem.innerText = carBrand
+				liElem.onclick = onSelectCarBrand
+				ulElem.appendChild(liElem)
+			})
+			document.body.appendChild(ulElem)
+		}
+	}
+
+	function run() {
+		searchElem.addEventListener('input', onInput)
+		document.addEventListener('click', clearResults)
+	}
+
+	run()
 })()
