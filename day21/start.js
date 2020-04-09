0cc23e3af9ab2089e0fa5ee7ff1af6c8ee4fc6ed
zobry <apirat.chiwa@gmail.com>
2020-04-09 12:28:45 +0700

    infinite load data when scroll to bottom

--- day21/start.js
+++ day21/start.js
@@ -1,7 +1,19 @@
 ;(() => {
 	const key = 'key'
+	const loaderElem = document.querySelector('.loader')
 	let page = 1
+
+	function showLoader() {
+		loaderElem.classList.add('visible')
+	}
+
+	function hideLoader() {
+		loaderElem.classList.remove('visible')
+	}
+
 	async function displayImages() {
+		showLoader()
+
 		const result = await fetch(
 			`https://api.unsplash.com/photos/?client_id=${key}&page=${page}`
 		)
@@ -13,9 +25,29 @@
 
 			galleryElem.appendChild(imgElem)
 		})
+
+		hideLoader()
+		page += 1
+	}
+
+	function onScroll() {
+		// scrollTop ตำแหน่ง top ของหน้าจอที่ scroll อยู่ในปัจจุบัน
+		// clientHeight ความสูงของหน้าจอที่แสดงอยู่ปัจจุบัน
+		// scrollHeight ความสูงทั้งหมดของเว็บ
+		// scrollTop + clientHeight = scrollHeight เสมอ
+		const {
+			scrollTop,
+			clientHeight,
+			scrollHeight,
+		} = document.documentElement
+
+		if (scrollTop + clientHeight >= scrollHeight - 10) {
+			displayImages()
+		}
 	}
 
 	function run() {
+		document.addEventListener('scroll', onScroll)
 		displayImages()
 	}
 
