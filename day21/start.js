5d6f98cb9c4171171a917dc3644351b1d6b671ae
zobry <apirat.chiwa@gmail.com>
2020-04-09 12:17:16 +0700

    show images form api

--- day21/start.js
+++ day21/start.js
@@ -1,3 +1,23 @@
 ;(() => {
-	// เริ่มเขียนโค้ด
+	const key = 'key'
+	let page = 1
+	async function displayImages() {
+		const result = await fetch(
+			`https://api.unsplash.com/photos/?client_id=${key}&page=${page}`
+		)
+		const images = await result.json()
+		const galleryElem = document.querySelector('.gallery')
+		images.forEach((img) => {
+			const imgElem = document.createElement('img')
+			imgElem.src = img.urls.small
+
+			galleryElem.appendChild(imgElem)
+		})
+	}
+
+	function run() {
+		displayImages()
+	}
+
+	run()
 })()
